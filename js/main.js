9cc1f1c05ef40738f9fb5580f916d471e4c52c7d
Anastasiya Mamontova <79529646321@ya.ru>
2022-02-18 14:15:37 +0300

    header

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 $(function(){
     $('.header__slider').slick({
     dots: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="../images/prev.svg" alt=""></button>', 
-    nextArrow: '<button type="button" class="slick-next"><img src="../images/next.svg" alt=""></button>'
+    prevArrow: '<button type="button" class="slick-prev"> Previous </button>', 
+    nextArrow: '<button type="button" class="slick-next"> Next </button>',
+    fade: true
 
     });
 
