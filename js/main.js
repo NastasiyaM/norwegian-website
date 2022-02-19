19c7feda118ae7fe77c54bd150000d6dc14a7cd8
Anastasiya Mamontova <79529646321@ya.ru>
2022-02-19 16:26:39 +0300

    Section product/ seafood content

--- js/main.js
+++ js/main.js
@@ -7,5 +7,21 @@ $(function(){
 
     });
 
+    $('.product__name').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    asNavFor: '.product__content',
+    focusOnSelect: true, //переключает слайды 
+    vertical: true,
+    prevArrow: '<button type="button" class="product-prev"> Previous </button>', 
+    nextArrow: '<button type="button" class="product-next"> Next </button>'
+
+    });
+    $('.product__content').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.product__name',
+    fade: true //способ анимации
+    });
 
 });
